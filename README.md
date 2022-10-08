06bf2851f753bcd6d82bdd987abfe2cbea974738
Douglas Rodrigues <douglas.fraga@gmail.com>
2022-10-08 19:48:48 -0300

    first version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # sentweet
 Sentiment Analysis for Twitter posts
 
-Twitter API credentials must be read from a external file
+Twitter API credentials must be read from a external file (ex: Login.txt)
--- sentweet.py
+++ sentweet.py
@@ -8,3 +8,88 @@ import re
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
+# Load Twitter API Credentials
+log = pd.read_csv('Login.txt')
+
+consumerKey = log['key'][0]
+consumerSecret = log['key'][1]
+bearerToken = log['key'][2]
+accessToken = log['key'][3]
+accessTokenSecret = log['key'][4]
+
+# Create the authentication object
+authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret)
+
+# Set the access token and access token secret
+authenticate.set_access_token(accessToken, accessTokenSecret)
+
+# Create the API object while passing in tne auth information
+api = tweepy.API(authenticate, wait_on_rate_limit = True)
+
+
+# Extract 100 tweets from the twitter user
+posts = api.user_timeline(screen_name = "MarkRuffalo", count = 100, tweet_mode = "extended")
+
+# Print the last 5 tweets from the account
+#for k, tweet in enumerate(posts[:5]):
+#  print(f'{k+1}) {tweet.full_text}\n')
+
+# Create a dataframe with a column called Tweets
+df = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'] )
+
+# Clean the text
+def cleanTxt(text):
+  text = re.sub(r'@[A-Za-z0-9]+', '', text)
+  text = re.sub(r'#', '', text)
+  text = re.sub(r'RT[\s]+', '', text)
+  text = re.sub(r'https?:\/\/\S+', '', text)
+  text = re.sub(r':', '', text)
+
+  return text
+
+df['Tweets'] = df['Tweets'].apply(cleanTxt)
+
+
+# Create a function to get the subjectivity
+def getSubjectivity(text):
+  return TextBlob(text).sentiment.subjectivity
+
+# Create a function to get the polarity
+def getPolarity(text):
+  return TextBlob(text).sentiment.polarity
+
+# Create two new columns
+df['Subjectivity'] = df['Tweets'].apply(getSubjectivity)
+df['Polarity'] = df['Tweets'].apply(getPolarity)
+
+# Plot the Word Cloud
+allWords = ' '.join([twts for twts in df['Tweets']])
+wordCloud = WordCloud(width = 500, height = 300, random_state = 21, max_font_size = 119).generate(allWords)
+
+plt.imshow(wordCloud, interpolation = 'bilinear')
+plt.axis('off')
+plt.show()
+plt.close()
+
+# Create a function to compute the negative, neutral and positive analysis
+def getAnalysis(score):
+  if score < 0:
+    return 'Negative'
+  elif score == 0:
+    return 'Neutral'
+  else:
+    return 'Positive'
+
+df['Analysis'] = df['Polarity'].apply(getAnalysis)
+
+# Plot the polarity and subjectivity
+plt.figure(figsize=(8,6))
+for i in range(0,df.shape[0]):
+  plt.scatter(df['Polarity'][i], df['Subjectivity'][i], color='Blue')
+print(i)
+
+plt.title('Sentiment Analysis')
+plt.xlabel('Polarity')
+plt.ylabel('Subjectivity')
+
+
