8b5692322ce817d185a64516590c07fd6cd9a971
Douglas Rodrigues <douglas.fraga@gmail.com>
2022-10-09 23:51:06 -0300

    SNA script

--- sentweet.py
+++ sentweet.py
@@ -1,3 +1,8 @@
+"""
+https://www.youtube.com/watch?v=ujId4ipkBio
+https://www.youtube.com/watch?v=pgZcP852dMg
+"""
+
 # Import the libraries
 import tweepy
 from textblob import TextBlob
@@ -6,90 +11,175 @@ import pandas as pd
 import numpy as np
 import re
 import matplotlib.pyplot as plt
+import networkx as nx
 plt.style.use('fivethirtyeight')
 
-# Load Twitter API Credentials
-log = pd.read_csv('Login.txt')
 
-consumerKey = log['key'][0]
-consumerSecret = log['key'][1]
-bearerToken = log['key'][2]
-accessToken = log['key'][3]
-accessTokenSecret = log['key'][4]
+# Functions
+def tweet_con(credentials_file):
+    """Connect and authenticate Twitter API"""
+    
+    log = pd.read_csv(credentials_file)
 
-# Create the authentication object
-authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret)
+    consumerKey = log['key'][0]
+    consumerSecret = log['key'][1]
+    bearerToken = log['key'][2]
+    accessToken = log['key'][3]
+    accessTokenSecret = log['key'][4]
 
-# Set the access token and access token secret
-authenticate.set_access_token(accessToken, accessTokenSecret)
+    # Create the authentication object
+    authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret)
 
-# Create the API object while passing in tne auth information
-api = tweepy.API(authenticate, wait_on_rate_limit = True)
+    # Set the access token and access token secret
+    authenticate.set_access_token(accessToken, accessTokenSecret)
+
+    # Create the API object while passing in tne auth information
+    api = tweepy.API(authenticate, wait_on_rate_limit = True)
+
+    return api
 
 
-# Extract 100 tweets from the twitter user
-posts = api.user_timeline(screen_name = "MarkRuffalo", count = 100, tweet_mode = "extended")
+def cleanTxt(text):
+    """Filter undesirable characters"""
+    text = re.sub(r'@[A-Za-z0-9]+', '', text)
+    text = re.sub(r'#', '', text)
+    text = re.sub(r'RT[\s]+', '', text)
+    text = re.sub(r'https?:\/\/\S+', '', text)
+    text = re.sub(r':', '', text)
 
-# Print the last 5 tweets from the account
-#for k, tweet in enumerate(posts[:5]):
-#  print(f'{k+1}) {tweet.full_text}\n')
+    return text
 
+
+def getSubjectivity(text):
+    """Create a function to get the subjectivity"""
+    return TextBlob(text).sentiment.subjectivity
+
+
+def getPolarity(text):
+    """Create a function to get the polarity"""
+    return TextBlob(text).sentiment.polarity
+
+
+def getAnalysis(score):
+    """Compute the negative, neutral and positive analysis"""
+    if score < 0:
+      result = 'Negative'
+    elif score == 0:
+      result = 'Neutral'
+    else:
+      result = 'Positive'
+
+    return result
+
+
+# Connect to Twitter API
+api = tweet_con('Login.txt')
+
+
+# Extract information
+me = api.get_user(screen_name = '77_frota')
+
+user_list = [me.id]
+follower_list = []
+for user in user_list:
+    followers = []
+    try:
+        for page in tweepy.Cursor(api.get_follower_ids, user_id=user).pages():
+            followers.extend(page)
+            print(len(followers))
+    except tweepy.errors:
+        print("error")
+        continue
+    follower_list.append(followers)
+
+df = pd.DataFrame(columns=['source','target']) #Empty DataFrame
+df['target'] = follower_list[0] #Set the list of followers as the target column
+df['source'] = me.id #Set my user ID as the source
+
+
+user_list = list(df['target']) #Use the list of followers we extracted in the code above
+for userID in user_list:
+    print(userID)
+    followers = []
+    follower_list = []
+
+    # fetching the user
+    user = api.get_user(user_id = userID)
+
+    # fetching the followers_count
+    followers_count = user.followers_count
+
+    try:
+        for page in tweepy.Cursor(api.get_follower_ids, user_id=userID).pages():
+            followers.extend(page)
+            print(len(followers))
+            if followers_count >= 5000: #Only take first 5000 followers
+                break
+    except tweepy.errors:
+        print("error")
+        continue
+    follower_list.append(followers)
+    temp = pd.DataFrame(columns=['source', 'target'])
+    temp['target'] = follower_list[0]
+    temp['source'] = userID
+    df = df.append(temp)
+    df.to_csv("networkOfFollowers.csv")
+
+
+
+
+
+G = nx.from_pandas_edgelist(df, 'source', 'target') #Turn df into graph
+pos = nx.spring_layout(G) #specify layout for visual
+
+
+f, ax = plt.subplots(figsize=(10, 10))
+plt.style.use('ggplot')
+nodes = nx.draw_networkx_nodes(G, pos,
+                               alpha=0.8)
+nodes.set_edgecolor('k')
+nx.draw_networkx_labels(G, pos, font_size=8)
+nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.2)
+plt.show()
+
+
+
+# Extract infos
+#posts = tweepy.Cursor(api.search_tweets, q='douglas').items(10)
+#posts = api.user_timeline(screen_name = "MarkRuffalo", count = 100, tweet_mode = "extended")
 # Create a dataframe with a column called Tweets
-df = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'] )
+#df = pd.DataFrame([tweet.text for tweet in posts], columns=['Tweets'] )
+
 
 # Clean the text
-def cleanTxt(text):
-  text = re.sub(r'@[A-Za-z0-9]+', '', text)
-  text = re.sub(r'#', '', text)
-  text = re.sub(r'RT[\s]+', '', text)
-  text = re.sub(r'https?:\/\/\S+', '', text)
-  text = re.sub(r':', '', text)
-
-  return text
-
-df['Tweets'] = df['Tweets'].apply(cleanTxt)
+#df['Tweets'] = df['Tweets'].apply(cleanTxt)
 
 
-# Create a function to get the subjectivity
-def getSubjectivity(text):
-  return TextBlob(text).sentiment.subjectivity
+# Create two new columns for subjectivity and polarity
+#df['Subjectivity'] = df['Tweets'].apply(getSubjectivity)
+#df['Polarity'] = df['Tweets'].apply(getPolarity)
 
-# Create a function to get the polarity
-def getPolarity(text):
-  return TextBlob(text).sentiment.polarity
-
-# Create two new columns
-df['Subjectivity'] = df['Tweets'].apply(getSubjectivity)
-df['Polarity'] = df['Tweets'].apply(getPolarity)
 
 # Plot the Word Cloud
-allWords = ' '.join([twts for twts in df['Tweets']])
-wordCloud = WordCloud(width = 500, height = 300, random_state = 21, max_font_size = 119).generate(allWords)
+#allWords = ' '.join([twts for twts in df['Tweets']])
+#wordCloud = WordCloud(width = 500, height = 300, random_state = 21, max_font_size = 119).generate(allWords)
+#
+#plt.imshow(wordCloud, interpolation = 'bilinear')
+#plt.axis('off')
+#plt.show()
+#plt.close()
 
-plt.imshow(wordCloud, interpolation = 'bilinear')
-plt.axis('off')
-plt.show()
-plt.close()
 
-# Create a function to compute the negative, neutral and positive analysis
-def getAnalysis(score):
-  if score < 0:
-    return 'Negative'
-  elif score == 0:
-    return 'Neutral'
-  else:
-    return 'Positive'
+# Compute the negative, neutral and positive analysis
+#df['Analysis'] = df['Polarity'].apply(getAnalysis)
 
-df['Analysis'] = df['Polarity'].apply(getAnalysis)
 
 # Plot the polarity and subjectivity
-plt.figure(figsize=(8,6))
-for i in range(0,df.shape[0]):
-  plt.scatter(df['Polarity'][i], df['Subjectivity'][i], color='Blue')
-print(i)
-
-plt.title('Sentiment Analysis')
-plt.xlabel('Polarity')
-plt.ylabel('Subjectivity')
-
+#plt.figure(figsize=(8,6))
+#for i in range(0,df.shape[0]):
+#  plt.scatter(df['Polarity'][i], df['Subjectivity'][i], color='Blue')
+#
+#plt.title('Sentiment Analysis')
+#plt.xlabel('Polarity')
+#plt.ylabel('Subjectivity')
 
---
+++ sna.py
@@ -0,0 +1,65 @@
+import tweepy
+import pandas as pd
+import networkx as nx
+from community import community_louvain
+import matplotlib.pyplot as plt
+
+
+def tweet_con(credentials_file):
+    """Connect and authenticate Twitter API"""
+    
+    log = pd.read_csv(credentials_file)
+
+    consumerKey = log['key'][0]
+    consumerSecret = log['key'][1]
+    bearerToken = log['key'][2]
+    accessToken = log['key'][3]
+    accessTokenSecret = log['key'][4]
+
+    # Create the authentication object
+    authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret)
+
+    # Set the access token and access token secret
+    authenticate.set_access_token(accessToken, accessTokenSecret)
+
+    # Create the API object while passing in tne auth information
+    api = tweepy.API(authenticate, wait_on_rate_limit = True)
+
+    return api
+
+
+api = tweet_con('Login.txt')
+
+df = pd.read_csv('networkOfFollowers.csv') #Read into a df
+G = nx.from_pandas_edgelist(df, 'source', 'target')
+nodes = G.number_of_nodes() #Find the total number of nodes in this graph
+
+G_sorted = pd.DataFrame(sorted(G.degree, key=lambda x: x[1], reverse=True))
+G_sorted.columns = ['names','degree']
+
+u = api.get_user(user_id = int(G_sorted['names'].iloc[0]))
+
+#G_tmp = nx.k_core(G, 10) #Exclude nodes with degree less than 10
+
+partition = community_louvain.best_partition(G)
+#Turn partition into dataframe
+partition1 = pd.DataFrame([partition]).T
+partition1 = partition1.reset_index()
+partition1.columns = ['names','group']
+print(G_sorted)
+
+combined = pd.merge(G_sorted, partition1, how='left', left_on="names",right_on="names")
+
+pos = nx.spring_layout(G)
+f, ax = plt.subplots(figsize=(10, 10))
+plt.style.use('ggplot')
+#cc = nx.betweenness_centrality(G2)
+nodes = nx.draw_networkx_nodes(G, pos,
+                               cmap=plt.cm.Set1,
+                               node_color=combined['group'],
+                               alpha=0.8)
+nodes.set_edgecolor('k')
+nx.draw_networkx_labels(G, pos, font_size=8)
+nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.2)
+plt.savefig('twitterFollowers.png')
+
